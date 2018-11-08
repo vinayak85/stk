c7258cf999d243d51d494807dc0a7a7822ed238c
vinayak85 <vinupatil9@gmail.com>
2018-11-08 15:02:23 +0530

    Update test2.py

--- stkapp/stkapp/doctype/test2/test2.py
+++ stkapp/stkapp/doctype/test2/test2.py
@@ -3,6 +3,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from upstox_api.api import *
 import frappe
 from frappe.model.document import Document
 import subprocess
@@ -16,7 +17,17 @@ from frappe.model.mapper import get_mapped_doc
 @frappe.whitelist()
 def auth():
 	c = frappe.db.sql("""select api_key,api_secret,accesstoken from 1bd3e0294da19198.tabtest where name='b2550d9017';""", as_dict=1);
-	frappe.msgprint(_(c[0].api_key));
+	key=c[0].api_key;
+	secret=c[0].api_secret;
+	access_token="";
+	
+	s = Session(key);
+        s.set_redirect_uri('http://upstox.com:3000');
+        s.set_api_secret(secret);
+	print('URL: %s\n' % s.get_login_url())
+	
+	
+	#key=frappe.msgprint(_(c[0].api_key));
 	#return c[0].name;
 	#dict = {'count': 0}
 	#dict['count'] = c[0].name;
