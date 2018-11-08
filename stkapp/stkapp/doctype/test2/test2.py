035ba17dceb0b19fb2413308366e8b6a9bd45df9
vinayak85 <vinupatil9@gmail.com>
2018-11-08 15:13:59 +0530

    Update test2.py

--- stkapp/stkapp/doctype/test2/test2.py
+++ stkapp/stkapp/doctype/test2/test2.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2018, vin and contributors
 # For license information, please see license.txt
-
-from __future__ import unicode_literals
 from upstox_api.api import *
+from __future__ import unicode_literals
+
 import frappe
 from frappe.model.document import Document
 import subprocess
